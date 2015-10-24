1e41110727154985635962302b3eccef8e6bf7b1
Divakar <divakarreddynaru6561@gmail.com>
2015-10-24 19:30:55 +0530

    Added the whether published column

--- Project/bcapp/models.py
+++ Project/bcapp/models.py
@@ -10,11 +10,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-	was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
-
-
+	
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
