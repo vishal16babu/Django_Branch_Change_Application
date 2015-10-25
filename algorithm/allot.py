d38d379be967cc20ae8da7274158e1a89798037e
pushyarag <pushyarag123456@gmail.com>
2015-10-25 19:57:30 +0530

    code for checking blocked people in set 2 while alloting

--- algorithm/allot.py
+++ algorithm/allot.py
@@ -46,16 +46,10 @@ else :
 	# print len(listofstuds)
 
 
-#dictionary of programs for easy access
+#dictionary of programs for easy access using tuple of name and cpi as key 
 	diction_progs = {}
 	for x in range(0, len(listofprogs)):
 		diction_progs[listofprogs[x][0]] = listofprogs[x]
-
-#dictionary of allotments for easy access
-	diction_allot = {}
-	for x in range(0, len(listofstuds)):
-		diction_allot[listofstuds[x][0]] = listofstuds[x]
-	print diction_allot['15xxxxxx7']
 
 #	print diction_progs
 
@@ -69,8 +63,8 @@ else :
 		listofprogs[x].append(int(round(a,0)))
 		listofprogs[x].append(int(round(b,0)))
 		listofprogs[x].append(0.0) #cpi of last alloted person
-		listofprogs[x].append(0.0) #highest cpi of  blocked persons
-		listofprogs[x].append('0') #roll no of highest cpi of  blocked persons
+		#listofprogs[x].append(0.0) #highest cpi of  blocked persons
+		listofprogs[x].append((0.0,[])) #names of highest cpi of  blocked persons
 		#print listofprogs[x]
 #listofprogs[x][0] prog name
 #listofprogs[x][1] sanchaned strength
@@ -85,7 +79,16 @@ else :
 	# print diction_progs
 
 #iterating based on the merit order
+	def find_element_in_list(element,list_element):
+        	try:
+            		index_element=list_element.index(element)
+            		return index_element
+        	except ValueError:
+            		return -1
+
+
 	def vacant(br):
+		#print br
 		curr = diction_progs[br]
 		if int(curr[2])<curr[4]:
 			return True
@@ -99,30 +102,51 @@ else :
 		else:
 			# updating the highest cpi of  blocked persons
 			curr1 = diction_progs[pq[y1]]
-			curr1[6]=max(curr1[6],pq[3])
+			#curr1[6]=max(curr1[6],pq[3])
+			if curr1[6][0]==float(pq[3]) and find_element_in_list(pq[1],curr1[6][1])==-1:
+				curr[6]=(curr1[6][0],curr1[6][1].append(pq[1]))
+			if curr1[6][0]<float(pq[3]):
+				curr[6]=(pq[3],curr1[6][1].append(pq[1]))
 			return False
 
 
-	def allot(p,b,f,d):
-		f.append([p[0],p[1],p[2],b])
+	def allot(p,y2,f,d):
+		#f.append([p[0],p[1],p[2],b])
 #updating the current strenght of the branch		
-		d[b][2]=str(int(d[b][2])+1)
+		d[p[y2]][2]=str(int(d[p[y2]][2])+1)
 #updating the current strenght of the prev branch		
 		d[p[2]][2]=str(int(d[p[2]][2])-1)
 # updating cpi of last alloted person			
-		d[b][5] = p[3]
-
+		d[p[2]][5] = p[3]
+#updating current branch
+		p[2]=str(p[y2])
+#removing the previous blocks in this and lower branches
+		for yo in range(y2, len(p)):
+			if p[yo]=='':
+				break
+			tt=d[p[yo]]
+			#print "no enthu"
+			#print tt	
+			if str(tt[6])==p[3]:
+				ind=find_element_in_list(pq[1],tt[6][1])
+				if ind!=-1:
+					temp=tt[6][1]
+					del temp[ind]
+				if len(tt[6][0])==0:
+					tt[6]=(0.0,[])	
+#removing the lesser preffered coices
+		p=p[0:y2]
+	changes=1
 	for x in range(0, len(listofstuds)):
 #case1 if cpi>9
 		if float(listofstuds[x][3]) >= 9.0:
-			#print listofstuds[x][3]
 			for y in range(0, len(listofstuds[x])-5):
 				if listofstuds[x][y+5] == "":
 					break
 				#print diction_progs[listofstuds[x][y+5]]
 #if seats are vacant or last person alloted has same cpi then allot				
 				if vacant(listofstuds[x][y+5]) or diction_progs[listofstuds[x][y+5]][5] == listofstuds[x][3]:
-					allot(listofstuds[x],listofstuds[x][y+5],final,diction_progs)
+					allot(listofstuds[x],y+5,final,diction_progs)
 					#if y == 0:
 					
 					break;						
@@ -138,11 +162,13 @@ else :
  						if vacant(listofstuds[x][y+5])  or diction_progs[listofstuds[x][y+5]][5] == listofstuds[x][3]:
 #if the seat is not blocked
  							if diction_progs[listofstuds[x][y+5]][6] < listofstuds[x][3]:
- 								allot(listofstuds[x],listofstuds[x][y+5],final,diction_progs)
+ 								allot(listofstuds[x],y+5,final,diction_progs)
  								if diction_progs[listofstuds[x][y+5]][6] == listofstuds[x][3]:
  									diction_progs[listofstuds[x][y+5]][6] = 0
 								break;						
  	
+
+
 	# for x in range(0, len(final)):
 	# 	print final[x]
 	# 	print x
