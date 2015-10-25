048d06ac576a2c98e44d8effd0e80cb612884a92
pushyarag <pushyarag123456@gmail.com>
2015-10-25 17:00:43 +0530

    input taken from input_programms.csv and input folder added

--- Project/bcapp/models.py
+++ Project/bcapp/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.utils import timezone
-
+import sys
+import csv
 
 class Person(models.Model):
     BRANCH_CHOICES = ((str(1), "GE"),(str(2), "OBC"),(str(3),"SC"),(str(4),"ST"),(str(5),"PwD"),)
+    listofprogs = []
+    filename = "../input/input_programmes.csv"
+    spamReader = csv.reader(open(filename))
+    for row in spamReader:          
+            listofprogs.append(row) 
+    BRANCH_CHOICES = [(str(x+1),listofprogs[x][0]) for x in range(0,len(listofprogs))]
     CAT_CHOICES    = ((str(1), "GE"),(str(2), "OBC"),(str(3),"SC"),(str(4),"ST"),(str(5),"PwD"),)
     roll_number    = models.IntegerField(default=150050049)
     name	       = models.CharField(max_length=200)
